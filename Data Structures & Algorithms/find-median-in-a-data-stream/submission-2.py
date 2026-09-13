1df264d705cd75baac1af8f537bb3b4a9e3a0875
justinwong2 <166791981+justinwong2@users.noreply.github.com>
2026-09-13 18:41:21 +0800

    Add: find-median-in-a-data-stream - submission-2

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-2.py
@@ -0,0 +1,42 @@
+class MedianFinder:
+
+    def __init__(self):
+        import heapq
+        self.minHeap = []
+        self.maxHeap = []
+
+    def addNum(self, num: int) -> None:
+        #if both are empty just add to the left which is the max
+        if len(self.minHeap) == len(self.maxHeap) == 0:
+            heapq.heappush_max(self.maxHeap, num)
+        else:
+            currVal = heapq.heappop_max(self.maxHeap)
+            if num < currVal:
+                heapq.heappush_max(self.maxHeap, num)
+            else:
+                heapq.heappush(self.minHeap, num)
+            heapq.heappush_max(self.maxHeap, currVal)
+            #if left side is imbalanced by more than 1
+            if len(self.maxHeap) + 1< len(self.minHeap):
+                #pop the right, add to left
+                temp = heapq.heappop(self.minHeap)
+                heapq.heappush_max(self.maxHeap, temp)
+            #i want my right side to be either balanced or larger
+            elif len(self.maxHeap) > len(self.minHeap):
+                temp = heapq.heappop_max(self.maxHeap)
+                heapq.heappush(self.minHeap, temp)
+
+
+    def findMedian(self) -> float:
+        if (len(self.maxHeap) + len(self.minHeap)) == 1:
+            return self.maxHeap[0]
+        if (len(self.maxHeap) + len(self.minHeap)) % 2 != 0:
+            return self.minHeap[0]
+        else:
+            temp1 = heapq.heappop_max(self.maxHeap)
+            heapq.heappush_max(self.maxHeap, temp1)
+            temp2 = heapq.heappop(self.minHeap)
+            heapq.heappush(self.minHeap, temp2)
+            return float((temp1 + temp2) / 2)
+        
+        
